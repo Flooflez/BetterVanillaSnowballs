bd6136d178f662c71a184affaec95ee1ddf084d8
Flooflez <maxmyta@gmail.com>
2023-01-14 18:25:51 +0800

    added new gamerule

--- src/main/java/net/better/snowball/fights/BetterSnowballFights.java
+++ src/main/java/net/better/snowball/fights/BetterSnowballFights.java
@@ -9,12 +9,15 @@ import org.slf4j.LoggerFactory;
 
 public class BetterSnowballFights implements ModInitializer {
 	public static GameRules.Key<GameRules.IntRule> SNOWBALL_DAMAGE;
+	public static GameRules.Key<GameRules.BooleanRule> SNOWBALLS_ONLY_DAMAGE_PLAYERS;
 
 	public static final Logger LOGGER = LoggerFactory.getLogger("bettersnowballfights");
 
 	@Override
 	public void onInitialize() {
 		SNOWBALL_DAMAGE = GameRuleRegistry.register("snowballDamage", GameRules.Category.MISC, GameRuleFactory.createIntRule(0,0));
+		SNOWBALLS_ONLY_DAMAGE_PLAYERS = GameRuleRegistry.register("snowballsOnlyDamagePlayers", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(false));
+
 
 		LOGGER.info("\"Hey, catch!\" Minecraft now loaded with 'Better Snowball Fights'");
 	}
--- src/main/java/net/better/snowball/fights/mixin/SnowballEntityMixin.java
+++ src/main/java/net/better/snowball/fights/mixin/SnowballEntityMixin.java
@@ -3,26 +3,37 @@ package net.better.snowball.fights.mixin;
 import net.better.snowball.fights.BetterSnowballFights;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.entity.projectile.thrown.SnowballEntity;
 import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(SnowballEntity.class)
-public abstract class SnowballEntityMixin extends Entity {
-    public SnowballEntityMixin(EntityType<?> type, World world) {
-        super(type, world);
+public abstract class SnowballEntityMixin extends ProjectileEntity{
+    public SnowballEntityMixin(EntityType<? extends ProjectileEntity> entityType, World world) {
+        super(entityType, world);
     }
 
-    @ModifyArg(method = "onEntityHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"), index = 1)
-    private float modifySnowballDamage(float damage){
+    @Redirect(method = "onEntityHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"))
+    private boolean injected(Entity entity, DamageSource source, float amount){
         GameRules gameRules = this.world.getGameRules();
 
-        if (gameRules.getInt(BetterSnowballFights.SNOWBALL_DAMAGE) <= 0) {
-            return damage;
+        int moddedDamage= gameRules.getInt(BetterSnowballFights.SNOWBALL_DAMAGE);
+
+        if(gameRules.getBoolean(BetterSnowballFights.SNOWBALLS_ONLY_DAMAGE_PLAYERS)){
+            float i = entity instanceof PlayerEntity ? moddedDamage : amount;
+            return entity.damage(DamageSource.thrownProjectile(this, this.getOwner()), i);
         }
-        return gameRules.getInt(BetterSnowballFights.SNOWBALL_DAMAGE);
+        else{
+            float i = moddedDamage == 0 ? amount : moddedDamage;
+            return entity.damage(DamageSource.thrownProjectile(this, this.getOwner()), i);
+        }
+
+
     }
 }
